c5e120927ebe16dd071c3c244eb0442427745d29
gaoxinru <gaoxinru@cmcm.com>
2025-09-17 22:57:16 +0800

    试解决联网搜索报错

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 class OpenRouterChat {
     constructor() {
-        this.searchApiUrl = 'https://hhhhmmm.vercel.app/api/search'; // 部署后需要替换成你自己的 Vercel URL
+        this.searchApiUrl = '/api/search'; // 部署后需要替换成你自己的 Vercel URL
         this.apiKey = localStorage.getItem('openrouter_api_key') || '';
         this.currentModel = '';
         this.availableModels = [];
